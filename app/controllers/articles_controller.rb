27dd8fa625abffba8995e10f1a753647e7ca1e3f
Jordan Levine <jordan@strofina.com>
2017-07-16 16:10:29 -0700

    Add error checking and display for new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,27 @@
 class ArticlesController < ApplicationController
+  def index
+   @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
   end
 
   def new
-
+    # this new article is a placeholder and will never be saved to the db;
+    # it is here b/c sometimes @article will be populated
+    # with data that producdd an error
+    @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
 
-    @article.save
-
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end 
   end
 
 
